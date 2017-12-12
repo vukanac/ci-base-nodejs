ed8c49ed35f0bf051c50a729a3bd558ace18bf85
Vladimir Vukanac <vukanac@gmail.com>
2017-12-12 22:34:46 +0100

    Add autogenerated eslint tests for all js files

---
+++ test/eslint-test.js
@@ -0,0 +1,36 @@
+// eslint-test.js
+const glob = require('glob');
+const CLIEngine = require('eslint').CLIEngine;
+const assert = require('chai').assert;
+
+const paths = glob.sync('./+(src|server|seeders|models|migrations|config|client/src)/**/*.js');
+const engine = new CLIEngine({
+  envs: [ 'node', 'mocha' ],
+  useEslintrc: true,
+});
+
+const results = engine.executeOnFiles(paths).results;
+
+describe('ESLint', () => {
+  results.forEach(result => generateTest(result));
+});
+
+function generateTest(result) {
+  const { filePath, messages } = result;
+
+  it(`validates ${filePath}`, () => {
+    if (messages.length > 0) {
+      assert.fail(false, true, formatMessages(messages));
+    }
+  });
+}
+
+function formatMessages(messages) {
+  const errors = messages.map(message =>
+    `${message.line}:${message.column}`
+      + ` ${message.message.slice(0, -1)}`
+      + ` - ${message.ruleId}\n`
+  );
+
+  return `\n${errors.join('')}`;
+}
